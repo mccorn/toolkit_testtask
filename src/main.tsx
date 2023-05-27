e5789f6e0c719f86c417ffd6b146c2666242e5cb
Markeev Andrew <mccorn13@gmail.com>
2023-05-27 12:52:39 +0300

    change token

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,15 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
+// const token = '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';
+// const token = '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';
+const token = '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';
+
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   headers: {
     // authorization: `Bearer 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`,
-    authorization: `Bearer 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 `,
+    authorization: `Bearer ${token} `,
     // authorization: `Bearer ${document.process.env.GRAPHQL_API_KEY}`,
   },
   cache: new InMemoryCache(),
